8a32b2161ce235e83be5f37d62b96bb2bd365878
Maxim Sushkov <max_123_99@mail.ru>
2024-02-11 23:00:03 +1000

    feat: add output to template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'angular-course-2023';
+  title = 'my-var-from-angular-course-Yuriy-Simonov';
+  user = {
+    name: 'Maxim',
+    surname: 'Sushkov',
+  };
+  tooltip = 'Подсказка tooltip для ссылки';
+
+  inlineStyleGreen = {
+    width: '50%',
+    background: 'green',
+  };
+  cssClass = 'blue';
 }
