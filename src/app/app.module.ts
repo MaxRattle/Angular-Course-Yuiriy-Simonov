8f0b24f6c272fab0a390aa7e4aab66a7d4d37566
Maxim Sushkov <max_123_99@mail.ru>
2024-02-13 00:20:33 +1000

    feat: learned ng-content

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AppComponent } from './app.component';
 import { PipesExamplePipe } from './pipes/pipes-example.pipe';
 import { SortDataPipe } from './pipes/sort-data.pipe';
 import { ChildComponent } from './components/child/child.component';
+import { NgContentExampleComponent } from './components/ng-content-example/ng-content-example.component';
 
 @NgModule({
-  declarations: [AppComponent, PipesExamplePipe, SortDataPipe, ChildComponent],
+  declarations: [AppComponent, PipesExamplePipe, SortDataPipe, ChildComponent, NgContentExampleComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
